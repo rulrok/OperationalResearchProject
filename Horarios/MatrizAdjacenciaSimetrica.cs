a418accc3e97417a136450c27618cf856ee9a2fe
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-05-22 12:10:40 -0300

    Extrai class para arquivo próprio

---
+++ Horarios/MatrizAdjacenciaSimetrica.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPO
+{
+    class MatrizAdjacenciaSimetrica<T>
+    {
+        T[] matrizLinear;
+
+        public int N { get; private set; }
+
+        public int LinearSize
+        {
+            get
+            {
+                return matrizLinear.Length;
+            }
+        }
+
+        public MatrizAdjacenciaSimetrica(int dimensao)
+        {
+            N = dimensao;
+            matrizLinear = new T[(N * (N + 1)) / 2];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return matrizLinear[index];
+            }
+        }
+
+        public T this[int i, int j]
+        {
+            get
+            {
+                return Get(i, j);
+            }
+            set
+            {
+                Set(i, j, value);
+            }
+        }
+
+        public T Get(int i, int j)
+        {
+            return matrizLinear[CalculaIndiceLinear(i, j)];
+        }
+
+        public T Set(int i, int j, T novoValor)
+        {
+            int indice = CalculaIndiceLinear(i, j);
+            T valorAntigo = matrizLinear[indice];
+            matrizLinear[indice] = novoValor;
+
+            return valorAntigo;
+        }
+
+        private int CalculaIndiceLinear(int i, int j)
+        {
+
+            if (i > j)
+            {
+                var aux = i;
+                i = j;
+                j = aux;
+            }
+
+            return (N * i) + j - ((i * (i + 1)) / 2);
+        }
+
+        public string ToString(Func<T, string> transformer)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    var value = transformer(this[i, j]);
+                    sb.Append(value).Append(" ");
+                }
+                sb.Append("\n");
+            }
+
+
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            //Transformador trivial
+            return ToString((t) => t.ToString());
+        }
+
+    }
+}
