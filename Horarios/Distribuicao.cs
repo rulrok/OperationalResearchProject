f664b2f81841f92adfdad6fa2d48fe5af72b9767
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-05-22 12:31:39 -0300

    Adiciona base inicial para código

--- Horarios/Distribuicao.cs
+++ Horarios/Distribuicao.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ILOG.CPLEX;
+using Plotter;
 
 namespace ProjetoPO
 {
@@ -30,10 +32,56 @@ namespace ProjetoPO
 
         }
 
-        private static void Solve(string file)
+        private static void Solve(string filePath)
         {
+            var points = ReadPoints(filePath);
+
+            var matrix = AssembleMatrix(points);
+
+            var model = new Cplex();
+
             throw new NotImplementedException();
         }
 
+        static List<PointD> ReadPoints(string filePath)
+        {
+
+            var lines = File.ReadAllLines(filePath);
+            var chartPoints = new List<PointD>(int.Parse(lines[0]));
+
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                //TODO Implementar a leitura
+
+                var X = 0;
+                var Y = 0;
+                var point = new PointD { X = X, Y = Y };
+                chartPoints.Add(point);
+            }
+
+            return chartPoints;
+
+        }
+
+        private static object AssembleMatrix(List<PointD> points)
+        {
+            var matrix = new MatrizAdjacenciaSimetrica<double>(points.Count);
+
+            for (int i = 0; i < matrix.N - 1; i++)
+            {
+                for (int j = i + 1; j < matrix.N; j++)
+                {
+                    matrix.Set(i, j, Distance(points[i], points[j]));
+                }
+            }
+
+            return matrix;
+        }
+
+        private static double Distance(PointD pointD1, PointD pointD2)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
