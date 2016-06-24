02bafe3dab2a854ced5740d03b1e86d9834270b2
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-06-24 13:30:57 -0300

    Adiciona assinatura de métodos externos em classe para esse fim

---
+++ Horarios/NativeMethods.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPO
+{
+    class NativeMethods
+    {
+
+        public struct CnstrRecord
+        {
+            int CType; /* Constraint Type. */
+            int Key;
+            int IntListSize;
+            IntPtr IntList;
+            int ExtListSize;
+            IntPtr ExtList;
+            int CListSize;
+            IntPtr CList;
+            IntPtr CoeffList;
+            int A, B, L; /* For MSTARs: Lambda=L/B, Sigma=A/B. */
+            double RHS;
+            int BranchLevel;
+            int GlobalNr;
+        }
+
+
+        public struct CnstrMgrRecord
+        {
+            public CnstrRecord CPL;
+            public int Dim;
+            public int Size;
+        }
+
+        [DllImport("CVRPSEP.dll")]
+        public static extern void CAPSEP_SeparateCapCuts(int NoOfCustomers,
+                    ref int Demand,
+                    int CAP,
+                    int NoOfEdges,
+                    ref int EdgeTail,
+                    ref int EdgeHead,
+                    ref double EdgeX,
+                    ref CnstrMgrRecord CMPExistingCuts,
+                    int MaxNoOfCuts,
+                    double EpsForIntegrality,
+                    ref char IntegerAndFeasible,
+                    ref double MaxViolation,
+                    ref CnstrMgrRecord CutsCMP);
+    }
+}
