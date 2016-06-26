0ac25cb2042df7e99e24f46b4f10f29b92becd50
Reuel Ramos Ribeiro <rulrok@gmail.com>
2016-06-26 14:57:23 -0300

    Extrai classe para arquivo próprio.

--- Horarios/Distribuicao.cs
+++ Horarios/Distribuicao.cs
@@ -2,188 +2,15 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using ILOG.Concert;
 using ILOG.CPLEX;
 using Plotter;
 using static Plotter.GraphPlotter;
-using System.Runtime.InteropServices;
-using System.Threading;
-using Microsoft.Win32.SafeHandles;
-using System.Diagnostics;
 
 namespace ProjetoPO
 {
-    public class NamedPipeServer
-    {
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern SafeFileHandle CreateNamedPipe(
-           String pipeName,
-           uint dwOpenMode,
-           uint dwPipeMode,
-           uint nMaxInstances,
-           uint nOutBufferSize,
-           uint nInBufferSize,
-           uint nDefaultTimeOut,
-           IntPtr lpSecurityAttributes);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern int ConnectNamedPipe(
-           SafeFileHandle hNamedPipe,
-           IntPtr lpOverlapped);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern int DisconnectNamedPipe(
-           SafeFileHandle hNamedPipe);
-
-        public const uint DUPLEX = (0x00000003);
-        public const uint FILE_FLAG_OVERLAPPED = (0x40000000);
-
-        public class Client
-        {
-            public SafeFileHandle handle;
-            public FileStream stream;
-        }
-
-        public const int BUFFER_SIZE = 100;
-        public Client clientse = null;
-
-        public string pipeName;
-        Thread listenThread;
-        SafeFileHandle clientHandle;
-        public int ClientType;
-
-        public NamedPipeServer(string PName, int Mode)
-        {
-            pipeName = PName;
-            ClientType = Mode;//0 Reading Pipe, 1 Writing Pipe
-
-        }
-
-        public void Start()
-        {
-            this.listenThread = new Thread(new ThreadStart(ListenForClients));
-            this.listenThread.Start();
-        }
-
-        private void ListenForClients()
-        {
-            while (true)
-            {
-
-                clientHandle = CreateNamedPipe(this.pipeName, DUPLEX | FILE_FLAG_OVERLAPPED, 0, 255, BUFFER_SIZE, BUFFER_SIZE, 0, IntPtr.Zero);
-
-                //could not create named pipe
-                if (clientHandle.IsInvalid)
-                    return;
-
-                int success = ConnectNamedPipe(clientHandle, IntPtr.Zero);
-
-                //could not connect client
-                if (success == 0)
-                    return;
-
-                clientse = new Client();
-                clientse.handle = clientHandle;
-                clientse.stream = new FileStream(clientse.handle, FileAccess.ReadWrite, BUFFER_SIZE, true);
-
-                if (ClientType == 0)
-                {
-                    Thread readThread = new Thread(new ThreadStart(Read));
-                    readThread.Start();
-                }
-            }
-        }
-
-        private void Read()
-        {
-            //Client client = (Client)clientObj;
-            //clientse.stream = new FileStream(clientse.handle, FileAccess.ReadWrite, BUFFER_SIZE, true);
-            byte[] buffer = null;
-            ASCIIEncoding encoder = new ASCIIEncoding();
-
-            while (true)
-            {
-
-                int bytesRead = 0;
-
-                try
-                {
-                    buffer = new byte[BUFFER_SIZE];
-                    bytesRead = clientse.stream.Read(buffer, 0, BUFFER_SIZE);
-                }
-                catch
-                {
-                    //read error has occurred
-                    break;
-                }
-
-                //client has disconnected
-                if (bytesRead == 0)
-                    break;
-
-                //fire message received event
-                //if (this.MessageReceived != null)
-                //    this.MessageReceived(clientse, encoder.GetString(buffer, 0, bytesRead));
-
-                int ReadLength = 0;
-                for (int i = 0; i < BUFFER_SIZE; i++)
-                {
-                    if (buffer[i].ToString("x2") != "cc")
-                    {
-                        ReadLength++;
-                    }
-                    else
-                        break;
-                }
-                if (ReadLength > 0)
-                {
-                    byte[] Rc = new byte[ReadLength];
-                    Buffer.BlockCopy(buffer, 0, Rc, 0, ReadLength);
-
-                    Console.WriteLine("C# App: Received " + ReadLength + " Bytes: " + encoder.GetString(Rc, 0, ReadLength));
-                    buffer.Initialize();
-                }
-
-            }
-
-            //clean up resources
-            clientse.stream.Close();
-            clientse.handle.Close();
-
-        }
-
-        public void SendMessage(byte[] bytesToSend, Client client)
-        {
-
-            if (client != null && client.stream.CanWrite)
-            {
-                client.stream.Write(bytesToSend, 0, bytesToSend.Length);
-                client.stream.Flush();
-            }
-
-
-        }
-
-        public void StopServer()
-        {
-            //clean up resources
-
-            if (this.clientHandle != null)
-            {
-                DisconnectNamedPipe(this.clientHandle);
-            }
-
-            if (this.listenThread != null)
-            {
-
-                this.listenThread.Abort();
-            }
-
-
-        }
-
-    }
 
     struct Customer
     {
@@ -193,7 +20,6 @@ namespace ProjetoPO
 
         public int Demand { get; set; }
     }
-
 
     class Distribuicao : Cplex.LazyConstraintCallback
     {
@@ -209,7 +35,7 @@ namespace ProjetoPO
 
 
             Console.WriteLine("==========================================");
-            Console.WriteLine("\tCorta sub-tours");
+            Console.WriteLine("==\tCorta sub-tours                     ==");
             Console.WriteLine("==========================================");
             Console.WriteLine();
 
@@ -280,7 +106,7 @@ namespace ProjetoPO
                 MyCutsCMP.Size = 0;
             } while (true);
 
-            return;
+            return; //TODO Remove this when the above code starts working
 
             foreach (var component in components)
             {
---
+++ Horarios/NamedPipeServer.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using Microsoft.Win32.SafeHandles;
+
+namespace ProjetoPO
+{
+    public class NamedPipeServer
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern SafeFileHandle CreateNamedPipe(
+           String pipeName,
+           uint dwOpenMode,
+           uint dwPipeMode,
+           uint nMaxInstances,
+           uint nOutBufferSize,
+           uint nInBufferSize,
+           uint nDefaultTimeOut,
+           IntPtr lpSecurityAttributes);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern int ConnectNamedPipe(
+           SafeFileHandle hNamedPipe,
+           IntPtr lpOverlapped);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern int DisconnectNamedPipe(
+           SafeFileHandle hNamedPipe);
+
+        public const uint DUPLEX = (0x00000003);
+        public const uint FILE_FLAG_OVERLAPPED = (0x40000000);
+
+        public class Client
+        {
+            public SafeFileHandle handle;
+            public FileStream stream;
+        }
+
+        public const int BUFFER_SIZE = 100;
+        public Client clientse = null;
+
+        public string pipeName;
+        Thread listenThread;
+        SafeFileHandle clientHandle;
+        public int ClientType;
+
+        public NamedPipeServer(string PName, int Mode)
+        {
+            pipeName = PName;
+            ClientType = Mode;//0 Reading Pipe, 1 Writing Pipe
+
+        }
+
+        public void Start()
+        {
+            this.listenThread = new Thread(new ThreadStart(ListenForClients));
+            this.listenThread.Start();
+        }
+
+        private void ListenForClients()
+        {
+            while (true)
+            {
+
+                clientHandle = CreateNamedPipe(this.pipeName, DUPLEX | FILE_FLAG_OVERLAPPED, 0, 255, BUFFER_SIZE, BUFFER_SIZE, 0, IntPtr.Zero);
+
+                //could not create named pipe
+                if (clientHandle.IsInvalid)
+                    return;
+
+                int success = ConnectNamedPipe(clientHandle, IntPtr.Zero);
+
+                //could not connect client
+                if (success == 0)
+                    return;
+
+                clientse = new Client();
+                clientse.handle = clientHandle;
+                clientse.stream = new FileStream(clientse.handle, FileAccess.ReadWrite, BUFFER_SIZE, true);
+
+                if (ClientType == 0)
+                {
+                    Thread readThread = new Thread(new ThreadStart(Read));
+                    readThread.Start();
+                }
+            }
+        }
+
+        private void Read()
+        {
+            //Client client = (Client)clientObj;
+            //clientse.stream = new FileStream(clientse.handle, FileAccess.ReadWrite, BUFFER_SIZE, true);
+            byte[] buffer = null;
+            ASCIIEncoding encoder = new ASCIIEncoding();
+
+            while (true)
+            {
+
+                int bytesRead = 0;
+
+                try
+                {
+                    buffer = new byte[BUFFER_SIZE];
+                    bytesRead = clientse.stream.Read(buffer, 0, BUFFER_SIZE);
+                }
+                catch
+                {
+                    //read error has occurred
+                    break;
+                }
+
+                //client has disconnected
+                if (bytesRead == 0)
+                    break;
+
+                //fire message received event
+                //if (this.MessageReceived != null)
+                //    this.MessageReceived(clientse, encoder.GetString(buffer, 0, bytesRead));
+
+                int ReadLength = 0;
+                for (int i = 0; i < BUFFER_SIZE; i++)
+                {
+                    if (buffer[i].ToString("x2") != "cc")
+                    {
+                        ReadLength++;
+                    }
+                    else
+                        break;
+                }
+                if (ReadLength > 0)
+                {
+                    byte[] Rc = new byte[ReadLength];
+                    Buffer.BlockCopy(buffer, 0, Rc, 0, ReadLength);
+
+                    Console.WriteLine("C# App: Received " + ReadLength + " Bytes: " + encoder.GetString(Rc, 0, ReadLength));
+                    buffer.Initialize();
+                }
+
+            }
+
+            //clean up resources
+            clientse.stream.Close();
+            clientse.handle.Close();
+
+        }
+
+        public void SendMessage(byte[] bytesToSend, Client client)
+        {
+
+            if (client != null && client.stream.CanWrite)
+            {
+                client.stream.Write(bytesToSend, 0, bytesToSend.Length);
+                client.stream.Flush();
+            }
+
+
+        }
+
+        public void StopServer()
+        {
+            //clean up resources
+
+            if (this.clientHandle != null)
+            {
+                DisconnectNamedPipe(this.clientHandle);
+            }
+
+            if (this.listenThread != null)
+            {
+
+                this.listenThread.Abort();
+            }
+
+
+        }
+
+    }
+
+}
